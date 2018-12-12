dce3496e98d043932dd76dc4dbf6e7e4d37bd14e
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-12 09:50:08 +0100

    Exercise 5

---
+++ src/main/java/Lexicon_Java_victor/Exercise_5/App.java
@@ -0,0 +1,21 @@
+package Lexicon_Java_victor.Exercise_5;
+
+import java.util.Scanner;
+
+public class App 
+{
+	
+	static Scanner scan = new Scanner(System.in);
+	
+    public static void main( String[] args )
+    {
+        
+    
+    
+    	System.out.println("what is your name");
+    	String text = scan.nextLine();
+    	System.out.println("hello " + text);
+    
+    
+    }
+}
